e867583c719b88c376d4e6de5c2695639d15e83c
murat tologlu <mtologlu@hotmail.com>
2016-02-05 19:58:00 +0200

    added forms.py

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,9 @@ def index(request):
 """
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+
+from django.shortcuts import redirect
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -17,3 +20,21 @@ def post_detail(request, pk):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+"""
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+"""
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
